9808930e163e867e6f0774a010f153930460aeb7
mattb20 <matt_burgoyne@live.co.uk>
2023-10-25 13:56:49 +0100

    Add different paths

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
-import figlet from 'figlet'
+import figlet from 'figlet';
 
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
-    const body = figlet.textSync('Bonjour!')
-    return new Response(body);
+  fetch(req){
+    const url = new URL(req.url)
+    if(url.pathname === '/') {
+      const body = figlet.textSync('Welcome to my bun app')
+      return new Response(body);
+    }
+    if(url.pathname === '/about') {
+      return new Response('About me!')
+    }
+    return new Response('404')
   }
 });
 console.log(`Listening on PORT http://localhost:${server.port}`);
